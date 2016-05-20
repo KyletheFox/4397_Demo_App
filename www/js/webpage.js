0e3f2c06b99c6a7dc6f1472d99a6234fcd4e42df
KyletheFox <ko163@me.com>
2016-05-19 21:34:00 -0400

    Added some sytling and a bunch of stuff since the last commit

---
+++ www/js/webpage.js
@@ -0,0 +1,30 @@
+
+$(document).ready(function() {
+
+	$('.icon-bar').colorCycle({
+		colors: ['#111', '#444', '#888', '#BBB', '#DDD', '#FFF' ]
+	});
+
+	$('.btn').colorCycle({
+		colors: ['#888', '#BBB', '#DDD', '#FFF' ]
+	});
+
+	$('#content-box').colorCycle({
+		colors: ['#111', '#444', '#888', '#BBB', '#DDD', '#FFF' ]
+	});
+
+	$('#nameLink').click(function() {
+		$('#nameLink').css("color", "#fff");
+	});
+
+	$('.navbar-toggle').click(function() {
+		$('.navbar-toggle').css("background-color", "#000");
+		
+	});
+
+	if($(window).width() <= 767) {
+		$('#navbar-link').remove();
+	}
+	
+
+});
