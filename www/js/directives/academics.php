c32729be532f7da681b66a44c816a48e529cb83a
KyletheFox <ko163@me.com>
2016-07-12 20:55:19 -0400

    Styled academic directive

--- www/js/directives/academics.php
+++ www/js/directives/academics.php
@@ -1,34 +1,70 @@
 <img src="img/temple-university-logo.svg">
-<ul>
-	<p>Temple Universtiy, College of Science and Technology</p>
-	<p>Bachelors of Science, Computer Science</p>
-	<p>Graduation: December 2016</p>
-	<p>GPA: 3.49</p>
-	<p>Deans List: Fall 2015, Spring 2016</p>
-</ul>
+<table>
+	<tr>
+		<th colspan="2">Temple Universtiy, College of Science and Technology</th>
+	</tr>
+	<tr>
+		<td colspan="2"><i>Bachelors of Science, Computer Science</i></td>
+	</tr>
+	<tr><td><img src="#" style="height:1px; width:1px;"></td></tr> <!-- Empty Row -->
+	<tr>
+		<th>Graduation:</th><td>December 2016</td>
+	</tr>
+	<tr>
+		<th>GPA:</th> <td>3.49</td>
+	</tr>
+	<tr>
+		<th>Deans List:</th> <td>Fall 2015, Spring 2016</td>
+	</tr>
+</table>
 
 <img src="img/BucksLogo.png">
-<ul>
-	<p>Bucks County Community College</p>
-	<p>Associates of Arts, Computer Science</p>
-	<p>Graduation: May 2014</p>
-	<p>GPA: 3.54</p>
-	<p>Deans List: Fall 2012, Spring 2013, Fall 2013</p>
-</ul>
+<table>
+	<tr>
+		<th colspan="2">Bucks County Community College</th>
+	</tr>
+	<tr>
+		<td colspan="2"><i>Associates of Arts, Computer Science</i></th>
+	</tr>
+	<tr><td><img src="#" style="height:1px; width:1px;"></td></tr> <!-- Empty Row -->
+	<tr>
+		<th>Graduation:&nbsp;</th><td>May 2014</td>
+	</tr>
+	<tr>
+		<th>GPA:&nbsp;</th><td>3.54</td>
+	</tr>
+	<tr>
+		<th>Deans List:&nbsp;</th><td>Fall 2012, Spring 2013, Fall 2013</td>
+	</tr>
+</table>
 
-<img src="img/ru-logo.gif" style="height: 70px;">
-<ul>
-	<p>Rutgers University</p>
-	<p>Bachelors of Arts, Computer Science</p>
-	<p>Attended: January 2012 - May 2012</p>
-</ul>
+<img src="img/ru-logo.gif">
+<table>
+	<tr>
+		<th colspan="2">Rutgers University</th>
+	</tr>
+	<tr>
+		<td colspan="2"><i>Bachelors of Arts, Computer Science</i></td>
+	</tr>
+	<tr><td><img src="#" style="height:1px; width:1px;"></td></tr> <!-- Empty Row -->
+	<tr>
+		<th>Attended:&nbsp;</th><td>January 2012 - May 2012</td>
+	</tr>
+</table>
 
 <img src="img/Bloomsburg-logo.png" style="height: 70px;">
-<ul>
-	<p>Bloomsburg University</p>
-	<p>Bachelors of Arts, Criminal Justice</p>
-	<p>Attended: December 2014</p>
-</ul>
+<table>
+	<tr>
+		<th colspan="2">Bloomsburg University</th>
+	</tr>
+	<tr>
+		<td colspan="2"><i>Bachelors of Arts, Criminal Justice</i></td>
+	</tr>
+	<tr><td><img src="#" style="height:1px; width:1px;"></td></tr> <!-- Empty Row -->
+	<tr>
+		<th>Attended:&nbsp;</th><td>August 2008 - Demeber 2010</td>
+	</tr>
+</table>
 
 
 
